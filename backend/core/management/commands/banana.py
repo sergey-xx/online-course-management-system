afa73d58c3e9eb065d6f93597355e8f3946fd5f3
sergei-mazilin <mazilinsp@outlook.com>
2025-11-12 10:32:27 +0400

    add vulture config

--- backend/core/management/commands/banana.py
+++ backend/core/management/commands/banana.py
@@ -9,5 +9,11 @@ class Command(BaseCommand):
     help = "Testing command."
 
     def handle(self, *args, **options):
+        # del options
         course = Course.objects.first()
         send_object_to_group(ChannelGroup.NOTIFICATION, course)
+
+
+def handle(*args, **kwargs):
+    course = Course.objects.first()
+    send_object_to_group(ChannelGroup.NOTIFICATION, course)
